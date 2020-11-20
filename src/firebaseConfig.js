6c26bc716845e8d809bbac80b31ec130ee7f46c2
Diego Ferreira <diegoferreira1964@gmail.com>
2020-11-20 14:20:17 -0300

    Configuração inicial do Firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyCf8bO7PPHUnw8ohLYHAKbk7Nk3xdJzCbg",
+    authDomain: "whatsappclone-d5171.firebaseapp.com",
+    databaseURL: "https://whatsappclone-d5171.firebaseio.com",
+    projectId: "whatsappclone-d5171",
+    storageBucket: "whatsappclone-d5171.appspot.com",
+    messagingSenderId: "253096035352",
+    appId: "1:253096035352:web:33fb28b1e2509f6d506f6d"
+  };
