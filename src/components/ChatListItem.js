a84515a6e5d082de77502eb87f384118eb14f05b
Diego Ferreira <diegoferreira1964@gmail.com>
2020-11-26 22:56:33 -0300

    Adcionando a função de horas na lista de ultimas menssagens

--- src/components/ChatListItem.js
+++ src/components/ChatListItem.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState ,useEffect } from 'react';
 import './ChatListItem.css';
 
 export default ({onClick, active, data}) => {
+
+    const [time, setTime ] = useState('');
+
+    //Função para trazer a data na mensagem
+    useEffect(() => {
+        if(data.lastMessageDate > 0) {
+            let d = new Date(data.lastMessageDate.seconds * 1000);
+            let hours = d.getHours();
+            let minutes = d.getMinutes();
+            minutes = hours < 10 ? '0'+minutes : minutes;
+            setTime(`${hours}:${minutes}`);
+        }
+
+    }, [data])
+
     return (
         <div 
             //Condição para mostrar a mensagem na tela
@@ -12,7 +27,7 @@ export default ({onClick, active, data}) => {
             <div className="chatListItem--lines">
                 <div className="chatListItem--line">
                 <div className="chatListItem--name">{data.title}</div>
-                    <div className="chatListItem--date">19:25</div>
+    <div className="chatListItem--date">{time}</div>
                 </div>
                 <div className="chatListItem--line">
                     <div className="chatListItem--lastMsg">
